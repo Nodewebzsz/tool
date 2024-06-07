0fffb80a3bcb3ec1910b1def3dac74f0f11991fa
zszweb <zszxingchenid@gmail.com>
2024-06-07 11:56:08 +0800

    optimise

--- generate_image_array.js
+++ generate_image_array.js
@@ -1,35 +1,36 @@
 const fs = require('fs');
 const path = require('path');
 const baseUrl = 'https://raw.githubusercontent.com/Nodewebzsz/tool/main/IconSet/';
-function generateIconSetObject(folderPath,filename) {
+function generateIconSetObject(folderPath,folderName) {
   const files = fs.readdirSync(folderPath);
   const imageArray = [];
   files.forEach(file => {
     const extname = path.extname(file);
     if (extname === '.png') { // 只处理 .png 图片
       const name = path.basename(file, extname); // 获取文件名 (不含扩展名)
-      const url = baseUrl + filename + '/' + file;
+      const url = baseUrl + folderName + '/' + file;
+      console.log(url);
       imageArray.push({ name, url });
     }
   });
   // 创建包含图片数组的对象
   const iconSetObject = {
-    name: filename, // 自定义名称
+    name: folderName, // 自定义名称
     description: 'By Nodewebzsz', // 自定义描述
     icons: imageArray
   };
   return iconSetObject;
 }
 const folderPath = path.join(__dirname, 'IconSet');
-fs.readdir(folderPath, (err, files) => {
+fs.readdir(folderPath, (err, folders) => {
   if (err) {
     console.error(`Error reading folder: ${err.message}`);
     return;
   }
-  files
-    .filter((file) => fs.lstatSync(path.join(folderPath, file)).isDirectory())
-    .forEach((file) => {
-      const filePath = path.join(folderPath, file);
-      fs.writeFileSync(path.join(folderPath, `${file}.json`), JSON.stringify(generateIconSetObject(filePath, baseUrl, file), null, 2));
+  folders
+    .filter((folder) => fs.lstatSync(path.join(folderPath, folder)).isDirectory())
+    .forEach((folder) => {
+      const filePath = path.join(folderPath, folder);
+      fs.writeFileSync(path.join(folderPath, `${folder}.json`), JSON.stringify(generateIconSetObject(filePath, folder), null, 2));
     });
 });
